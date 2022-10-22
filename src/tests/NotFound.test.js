4bef50535eada8044ec70a2813a9097886b7afa0
Eddmafra <eduardo_mafra@hotmail.com>
2022-10-22 17:27:08 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import NotFound from '../pages/NotFound';
+
+describe('Tests <NotFound /> component', () => {
+  test('Tests if the page has a heading (h2) with the message: Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+    const notFoundText = screen.getByRole('heading', { level: 2, name: /page requested not found/i });
+    expect(notFoundText).toBeValid();
+  });
+  test('Tests if the page has a img with the src: https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    renderWithRouter(<NotFound />);
+    const notFoundImg = screen.getByRole('img');
+    expect(notFoundImg).toBeInTheDocument();
+    expect(notFoundImg).toHaveProperty('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
